b65b9a5fb1d26bef79771adce79684761558d2c2
Dilara <dilaraderinpinar1@gmail.com>
2021-09-14 22:22:55 +0300

    Add project files.

---
+++ AsyncDemo1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.YÖNTEM
+
+            //Task task1 = new Task(Process1);
+            //task1.Start();
+
+            //2.YÖNTEM
+            //Task task1 = Task.Factory.StartNew(Process1);
+
+            //3.YÖNTEM
+            //Task task1 = Task.Run(Process1); //hızlı çalıştırır.
+
+            //4.YÖNTEM çalışma esnasında bir metod oluşturulabilir
+            //Task task1 = Task.Run(()=> { //task1 diğerleri ile asenkron çalışır
+            //    Process1();
+            //    Process2();
+            //    //process1 ve 2 sıralı bir şekilde çalışacaktır.asenkron şekilde değil
+            //});
+
+            Task task1 = Task.Run(Process1);
+
+            task1.Wait(); //task1 in çalışmama olasılığını ortadan kaldırıp ikinci işleme geçer. çünkü ikinci işlemin birinci işlemin çıktısına ihtiyaç duydugu senaryolar olabilir.
+            Process2();
+            Console.ReadKey(); //herhangi bir tuşa basınca sonlansın console.
+
+        }
+        static void Process1()
+        {
+            Console.WriteLine("1.işleme başladın");
+            Console.WriteLine($"Thread no:  { Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static void Process2()
+        {
+            Console.WriteLine("2.işleme başladın");
+            Console.WriteLine($"Thread no:  { Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
---
+++ AsyncDemo3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ASYNC-AWAIT ILE ASENKRON METOTLAR
+            Console.WriteLine($"Main thread başladı. {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("Hello World!");
+            Process1Async();
+            Process2Async();
+            Process1();
+            Process2(); //main process1 ve process2 nin thread numaraları 1 dir. asenkron değil senkron çalışırlar.
+
+        }
+
+        static void Process1()
+        {
+            
+            Console.WriteLine($"İşlem 1 başladı. {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process1Async() //dönüş tipleri task olmalı.
+        {   //async ise asenkron bir process oldugunu belirtir.
+            //await asenktron operasyon kendi içinde sağlıklı bir süreç geçirmesini sağlar yani task.wait() ile aynı işlev görür.
+            //bu processteki tüm kodları buraya yazarsın:
+            await Task.Run(()=> {
+                
+                Console.WriteLine($"Asenkron İşlem 1 başladı. {Thread.CurrentThread.ManagedThreadId}");
+            });
+            
+        }
+
+        static void Process2()
+        {
+            Console.WriteLine($"İşlem 2 başladı. {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        static async Task Process2Async()
+        {
+            await Task.Run(() => {
+                Console.WriteLine($"Async İşlem 2 başladı. {Thread.CurrentThread.ManagedThreadId}");
+            });
+            
+        }
+    }
+}
